bd90d291ff94ed5fa1d31e965d1b89aa0966c6c3
MarwanDebbiche <marwan.debbiche@gmail.com>
2020-10-08 20:31:04 +0000

    Deploying to gh-pages from  @ aa24f674f1e5743a74687fd4d152bf823fdb3a10 🚀

---
+++ precache-manifest.587db5528794e83a27b11871292add4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bfcfbbd10c43be25996ff3ed147cfd",
+    "url": "/annotated_research_papers/index.html"
+  },
+  {
+    "revision": "9ab471f3c4e62557d22d",
+    "url": "/annotated_research_papers/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d160c32094d4902c4284",
+    "url": "/annotated_research_papers/static/js/2.d882fa93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/annotated_research_papers/static/js/2.d882fa93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab471f3c4e62557d22d",
+    "url": "/annotated_research_papers/static/js/main.8bc16166.chunk.js"
+  },
+  {
+    "revision": "028a4c7693c7c28c9fbc",
+    "url": "/annotated_research_papers/static/js/runtime-main.fd840a46.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f914fa715ebe5dbd62483fb71315f23c.js"
+  "/annotated_research_papers/precache-manifest.587db5528794e83a27b11871292add4e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/annotated_research_papers/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
